3f6586c8551f6541af8131b01f44ec558c60b6b3
leeutjie <lhbecker@outlook.com>
2021-12-15 07:00:56 +0200

    some more practice

--- app.R
+++ app.R
@@ -2,8 +2,15 @@
 library(shiny)
 
 # UI defines the user interface, the HTML webpage that humans interact with.
+# fluidPage is a layout function that sets up the basic visual structure of the page
 ui <- fluidPage(
-    "Hello, world!"
+    
+    # selectInput() is an input control that lets the user interact with the app by providing a value
+    selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+    
+    # verbatimTextOutput() and tableOutput() are output controls that tell Shiny where to put rendered output
+    verbatimTextOutput("summary"),
+    tableOutput("table")
 )
 
 # specifies the behaviour of our app
@@ -11,4 +18,4 @@ server <- function(input, output, session) {
 }
 
 # execute to construct and start a Shiny application from UI and server
-shinyApp(ui, server)
+shinyApp(ui, server) 
